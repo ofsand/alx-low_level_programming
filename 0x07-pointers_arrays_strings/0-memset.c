adf0b50e0494abd17656fc8c2c436be6af4a2f23
ofsand <zesoufian@gmail.com>
2023-07-05 17:14:58 -0700

    Correct task 0 v1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value *n points to.
- * @n: the number to modify.
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
 
-void reset_to_98(int *n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	*n = 98;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
